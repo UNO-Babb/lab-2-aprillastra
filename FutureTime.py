aa8f3666dcf93591b54c6adb0739baad51b9306f
aprillastra <klastra-bernal@unomaha.edu>
2025-02-02 22:28:18 +0000

    lab 2

--- FutureTime.py
+++ FutureTime.py
@@ -9,21 +9,25 @@ import datetime
 def main():
   #getting current time from system, storing to variable
   now = datetime.datetime.now()
-  currentHour = now.hour
+  currentHour = (now.hour - 6) 
   currentMinute = now.minute
 
   print (currentHour, currentMinute) #this is just for checking, we should delete it later
 
   #TODO:
   #Ask user for hours
+  hours = int(input("Enter the number of hours to add: "))
   #Ask user for minutes
+  minutes = int(input("Enter the number of minutes to add: "))
 
   #Calculate the time after the user-supplied time has passed.
-
+  time to add = timedelta(hours=hours, minutes=minutes)
+new time = (current time + time to add)
   #Do not use any if statements in calculating the time.
 
   #Output the future time in standard format "HH:MM"
-
+print("\nCurrent time:",) # 12-hour format with AM/PM
+print("Future time after adding", hours, "hours and", minutes, "minutes")
 
 if __name__ == '__main__':
   main()
--- Magic8Ball.py
+++ Magic8Ball.py
@@ -1,18 +1,26 @@
 #Magic8Ball.py
-#Name:
+#Name: April Lastra 
 #Date:
-#Assignment:
+#Assignment: Lab 2 
 
 #We will need random for this program, import to use this package.
 import random
-
 def main():
-  #Create a list of your responses.
-  print("Magic 8 Ball")
+response = ["yes, definitely.", "It is certain.", "Without a doubt."
+             "Most Likely.", "You may rely on it.", "As I see it, yes.", "Outlook good.",
+             "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", 
+             "Cannot predict now.", "Don't count on it.", "My reply is no.",
+             "My sources say no.", "Outlook not so good.", "Very doubtful."]
+  print("Hello, welcome to magic 8 ball!")
   #Prompt the user for their question.
 
   #Answer question randomly with one of the options from your earlier list.
-
-
+  length = len(answers)
+r = random.random() * 16 
+r = int(r) # cut off decimal values 
+# 0 - 1 and make it 0 - 15 ?
+print(r)
+response = answer(r)
+print(response)
 if __name__ == '__main__':
   main()
